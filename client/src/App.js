bf9e9b7659dd51ecf0d8e1c0092243ee5e9b88c8
Kobi96 <tobiasolveira@outlook.com>
2023-07-27 22:32:18 -0300

    api routing OK 27-7-23

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { Home, Landing, Form, Detail } from "./views";
+import NavBar from "./components/NavBar";
+import { Route, useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      {location.pathname !== "/" && <NavBar />}
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/create" component={Form} />
+      <Route exact path="/detail" component={Detail} />
+
+      <Route path="/home" render={() => <Home />} />
     </div>
   );
 }
