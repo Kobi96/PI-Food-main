e605988f536fe9a34956d3ae49595263e78df321
Kobi96 <tobiasolveira@outlook.com>
2023-07-31 18:25:22 -0300

    redux OK 31-7-23

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,5 +1,13 @@
-const Card = () => {
-  return <div></div>;
+import style from "./Card.module.css";
+
+const Card = (props) => {
+  return (
+    <div className={style.card}>
+      <p>Name: {props.name}</p>
+      <img src={props.image} alt="" />
+      <p>Diets:{props.diets.map((diet) => diet + " ")}</p>
+    </div>
+  );
 };
 
 export default Card;
